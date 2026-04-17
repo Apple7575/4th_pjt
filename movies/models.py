ec3f4afdc358845f24ef8daf063ae6bfe88a1eca
bsw1206 <toysw1206@gmail.com>
2026-04-17 14:07:58 +0900

    feat: pjt04 필수 기능 구현

--- movies/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Movie(models.Model):
-    title = models.CharField()
-    description = models.TextField()
-    director = models.CharField()
